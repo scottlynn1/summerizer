15fc49763ea623f39c561c314f0b7a65c7d22fe5
scottlynn1 <scottlynn234@yahoo.com>
2025-05-11 06:54:57 -0400

    add streaming http functionality and more styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Chart from 'chart.js/auto'
 import { Bar } from 'react-chartjs-2';
 import { Line } from 'react-chartjs-2';
 import './App.css'
-
+const states_abbr = {AL: 'Alabama', AK: 'Alaska', AZ: 'Arizona', AR: 'Arkansas', CA: 'California', CO: 'Colorado', CT: 'Connecticut', DE: 'Delaware',
+  FL: 'Florida', GA: 'Georgia', HI: 'Hawaii', ID: 'Idaho', IL: 'Illinois', IN: 'Indiana', IA: 'Iowa', KS: 'Kansas', KY: 'Kentucky', LA: 'Louisiana', ME: 'Maine', 
+  MD: 'Maryland', MA: 'Massachusetts', MI: 'Michigan', MN: 'Minnesota', MS: 'Mississippi', MO: 'Missouri', MT: 'Montana', NE: 'Nebraska', NV: 'Nevada',
+  NH: 'New Hampshire', NJ: 'New Jersey', NM: 'New Mexico', NY: 'New York', NC: 'North Carolina', ND: 'North Dakota', OH: 'Ohio', OK: 'Oklahoma', OR: 'Oregon', 
+  PA: 'Pennsylvania', RI:'Rhode Island', SC: 'South Carolina', SD: 'South Dakota', TN: 'Tennessee', TX: 'Texas', UT: 'Utah', VT: 'Vermont', VA: 'Virginia', WA: 'Washington',
+  WV: 'West Virginia', WI: 'Wisconsin', WY: 'Wyoming'};
 const API_BASE = import.meta.env.VITE_API_BASE_URL || '/api';
 let _csrfToken = null;
 
@@ -19,31 +24,63 @@ async function getCsrfToken() {
 }
 
 function ContainerComp() {
-  const [summary, setSummary] = useState([]);
+  const [summary, setSummary] = useState('');
   const [loading, setLoading] = useState('none');
+  const [isloading, setIsloading] = useState(false)
   const [linechartdata, setLinechartdata] = useState({});
   const [barchartdata, setBarchartdata] = useState({});
+  const [largestIncrease, setLargestIncrease] = useState(0);
+  const [largestDecrease, setLargestDecrease] = useState(0);
+  const [ifrom, setIfrom] = useState('--')
+  const [ito, setIto] = useState('--')
+  const [dfrom, setDfrom] = useState('--')
+  const [dto, setDto] = useState('--')
+  
+  let latestrequestid = useRef(0);
 
   const getSummary = async (object) => {
     setLoading('block')
-    fetch(`${API_BASE}/api/`, {
+    setSummary('')
+    setIsloading(true)
+    const requestid = ++latestrequestid.current;
+    const response = await fetch(`${API_BASE}/stream/`, {
       method: 'POST',
       body: JSON.stringify(object),
       headers: {'X-CSRFToken': await getCsrfToken(), 'Content-type': 'application/json; charset=UTF-8'},
       credentials: 'include'
-   }).then((response) => response.json()).then((data) => {
-          console.log(data)
-          setLoading('none');
-          setSummary(data['db']);
-          setBarchartdata(data['chartdata1']);
-          setLinechartdata(data['chartdata']);
-        })
-        .catch((err) => {
-          setLoading('none');
-          console.log(err.message);
-        }
-      )
-    };
+    })
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder('utf-8');
+  
+    const { value: value1 } = await reader.read();
+  if (value1) {
+    const data = decoder.decode(value1);
+    const obj = JSON.parse(data);
+    setBarchartdata(obj['chartdata1'])
+    setLinechartdata(obj['chartdata'])
+    const { increasingindices, maxIncrease } = getlargestIncrease(Object.values(obj['chartdata']))
+    setIfrom(Object.keys(obj['chartdata'])[increasingindices[0]])
+    setIto(Object.keys(obj['chartdata'])[increasingindices[1]])
+    setLargestIncrease(maxIncrease)
+    const { decreasingindices, maxDecrease } = getlargestDecrease(Object.values(obj['chartdata']))
+    setDfrom(Object.keys(obj['chartdata'])[decreasingindices[0]])
+    setDto(Object.keys(obj['chartdata'])[decreasingindices[1]])
+    setLargestDecrease(maxDecrease)
+    
+  }
+
+  const { value: value2 } = await reader.read();
+  if (value2 && latestrequestid.current === requestid) {
+    const data = decoder.decode(value2);
+    const obj = JSON.parse(data);
+    setSummary(obj['db'])
+    setLoading('none')
+    setIsloading(false)
+  }
+  };
+    
+
+
 
 
   return (
@@ -52,15 +89,15 @@ function ContainerComp() {
         User interface to query the starbucks reviews dataset
       </header>
       
-      <div className='flex flex-col min-[1570px]:flex-row min-[995px]:mx-[80px] items-center'>
-        <div className='flex-2 flex flex-col'>
-          <div className='flex flex-col min-[995px]:flex-row items-center'>
+      <div className='flex flex-col min-[1570px]:flex-row min-[995px]:mx-[20px] items-center justify-center'>
+        <div className='flex-3 flex flex-col mx-4'>
+          <div className='flex flex-col min-[995px]:flex-row items-center m-4'>
             <div className='box min-w-[400px] m-4 p-2'>
               <MyForm loading={loading} getSummary={getSummary}/>
             </div>
-            <div className='box w-full max-w-[650px] min-h-[410px] min-[995px]:min-w-[550px] m-4'>
+            <div className='box w-full max-w-[650px] min-h-[410px] min-[995px]:min-w-[550px] '>
               <h2 className='border-b-2'>Summary</h2>
-              <MySummary summary={summary}/>
+              <MySummary summary={summary} isloading={isloading}/>
             </div>
           </div>
           <div className='box m-4 max-w-[1085px]'>
@@ -73,46 +110,46 @@ function ContainerComp() {
               </div>
               <div className='flex-1 px-2 mt-4'>
                 <div className='p-2 my-2 text-2xl'>
-                  <p>Best Year: <span>{summary.length > 0 ? Math.max(...Object.values(linechartdata)).toFixed(2) : '--'}</span></p>
+                  <p className='text-data'>Best Year <span className='data-numbers'>{largestIncrease ? Math.max(...Object.values(linechartdata)).toFixed(2) : '--'}</span></p>
                 </div>
                 <div className='p-2 my-2 text-2xl border-t-1'>
-                  <p>Worst Year: <span>{summary.length > 0 ? Math.min(...Object.values(linechartdata)).toFixed(2) : '--'}</span></p>                  
+                  <p className='text-data'>Worst Year <span className='data-numbers'>{largestDecrease ? Math.min(...Object.values(linechartdata)).toFixed(2) : '--'}</span></p>                  
                 </div>
                 <div className='p-2 my-2 text-2xl border-t-1'>
-                  <p>Greatest Increase: </p>
-                  <p></p>
+                  <p className='text-data'>Greatest Increase  </p>
+                  <p className='data-numbers'>{largestIncrease.toFixed(2)} from {ifrom} to {ito}</p>
                 </div>
                 <div className='p-2 my-2 text-2xl border-t-1'>
-                  <p>Greatest Decrease: <span></span></p>
-                  <p></p>
+                  <p className='text-data'>Greatest Decrease  <span></span></p>
+                  <p className='data-numbers'>{largestDecrease.toFixed(2)} from {dfrom} to {dto}</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className='flex-1'>
-          <div className='box m-4 max-w-[1050px]'>
+        <div className='flex-2'>
+          <div className='box m-4 '>
             <h2 className='border-b-2'>Average Rating by State</h2>
-            <div className='flex flex-col min-[650px]:flex-row mt-4 max-w-126 h-184'>
-              <div className='chart flex-2 overflow-y-auto my-2 w-[350px] min-w-[200px] p-4 items-stretch'>
-                <div className='aspect-4/18 w-'>
+            <div className='flex flex-col min-[650px]:flex-row mt-4 h-184'>
+              <div className='chart flex-2 overflow-y-auto my-2 min-w-[200px] p-4 items-stretch max-w-76'>
+                <div className='aspect-4/18'>
                   <BarChart barchartdata={barchartdata}/>
                 </div>
               </div>
               <div className='flex-1 '>
                 <div>
-                  <p className='p-2 my-2 text-2xl border-t-1'>Top 3 States: <span></span></p>
-                  <p className='p-2 my-2 text-2xl border-t-1'>
-                    {Object.keys(barchartdata).slice(0,3).map((state, index) => (
-                      <p>{`${index + 1}. ${state}`}</p>
+                  <p className='p-2 my-2 text-2xl text-data'>Top 3 States<span></span></p>
+                  <div className='p-2 my-2 text-2xl border-t-1'>
+                    {Object.keys(barchartdata).slice(0,3).map((state) => (
+                      <p className='border-b-1 border-dashed p-2 data-numbers'>{`${states_abbr[state]}`}</p>
                     ))}
-                  </p>
-                  <p className='p-2 my-2 text-2xl border-t-1'>Bottom 3 States: <span></span></p>
-                  <p className='p-2 my-2 text-2xl border-t-1'>
-                    {Object.keys(barchartdata).slice(-3).map((state, index) => (
-                      <p>{`${index + 1}. ${state}`}</p>
+                  </div>
+                  <p className='p-2 my-2 text-2xl text-data'>Bottom 3 States<span></span></p>
+                  <div className='p-2 my-2 text-2xl border-t-1'>
+                    {Object.keys(barchartdata).slice(-3).reverse().map((state) => (
+                      <p className='border-b-1 border-dashed p-2 data-numbers'>{`${states_abbr[state]}`}</p>
                     ))}
-                  </p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -120,8 +157,8 @@ function ContainerComp() {
         </div>
       </div>
 
-      <footer class="footer bg-blue-100 p-2 text-center py-6 mt-6">
-        <p class="">© 2025 Scott Lynn. All rights reserved.</p>
+      <footer className="footer bg-blue-100 p-2 text-center py-6 mt-6">
+        <p className="">© 2025 Scott Lynn. All rights reserved.</p>
       </footer>
     </>
   )
@@ -141,7 +178,6 @@ function MyForm({loading, getSummary}) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({years, state, ratings, product})
     let fail = false
     if (Number(years.end) < Number(years.start)) {
       setFailsumbit('text-red-500')
@@ -196,14 +232,8 @@ function MyForm({loading, getSummary}) {
 
   const productSelect = (e) => setProduct(e.target.value)
 
-  const states_abbr = {AL: 'Alabama', AK: 'Alaska', AZ: 'Arizona', AR: 'Arkansas', CA: 'California', CO: 'Colorado', CT: 'Connecticut', DE: 'Delaware',
-    FL: 'Florida', GA: 'Georgia', HI: 'Hawaii', ID: 'Idaho', IL: 'Illinois', IN: 'Indiana', IA: 'Iowa', KS: 'Kansas', KY: 'Kentucky', LA: 'Louisiana', ME: 'Maine', 
-    MD: 'Maryland', MA: 'Massachusetts', MI: 'Michigan', MN: 'Minnesota', MS: 'Mississippi', MO: 'Missouri', MT: 'Montana', NE: 'Nebraska', NV: 'Nevada',
-    NH: 'New Hampshire', NJ: 'New Jersey', NM: 'New Mexico', NY: 'New York', NC: 'North Carolina', ND: 'North Dakota', OH: 'Ohio', OK: 'Oklahoma', OR: 'Oregon', 
-    PA: 'Pennsylvania', RI:'Rhode Island', SC: 'South Carolina', SD: 'South Dakota', TN: 'Tennessee', TX: 'Texas', UT: 'Utah', VT: 'Vermont', VA: 'Virginia', WA: 'Washington',
-    WV: 'West Virginia', WI: 'Wisconsin', WY: 'Wyoming'};
 
-  const yearslist = ['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024'];
+  const yearslist = ['2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024'];
 
   return (
       <form onSubmit={handleSubmit} className='flex flex-col text-center'>
@@ -212,21 +242,21 @@ function MyForm({loading, getSummary}) {
           <p style={{display: yearserror}} className={failsubmit}>End year cannot be less than start year</p>
           <div>
             <div className='m-2'>
-              <label className='m-1' for='start'>Starting Year</label>
-              <select onChange={onDateselect} className='border p-2 rounded-s-sx' id="start" name='start'>
-                {yearslist.map((year) => {
+              <label className='m-1' htmlFor='start'>Starting Year</label>
+              <select onChange={onDateselect} className='border p-2 rounded-s-sx input-bg' id="start" name='start'>
+                {yearslist.map((year, index) => {
                   return (
-                    <option value={year}>{year}</option>
+                    <option key={index} value={year}>{year}</option>
                   )
                 })}
               </select>
             </div>
             <div className='m-2'>
-              <label className='m-1' for='end'>Ending Year</label>
-              <select onChange={onDateselect} className='border p-2 rounded-s-sx' id="end" name='end' value={years.end}>
-                {yearslist.map((year) => {
+              <label className='m-1' htmlFor='end'>Ending Year</label>
+              <select onChange={onDateselect} className='border p-2 rounded-s-sx input-bg' id="end" name='end' value={years.end}>
+                {yearslist.map((year, index) => {
                   return (
-                    <option value={year}>{year}</option>
+                    <option key={index} value={year}>{year}</option>
                   )
                 })}
               </select>
@@ -234,12 +264,12 @@ function MyForm({loading, getSummary}) {
           </div>
         </div>
         <div className='font-semibold p-2'>
-        <label className='p-2' for='state'>State</label>
-        <select onChange={onStateselect} className='border p-2 rounded-s-sx' id="state" name='state'>
+        <label className='p-2' htmlFor='state'>State</label>
+        <select onChange={onStateselect} className='border p-2 rounded-s-sx input-bg' id="state" name='state'>
           <option value='all'>All</option>
-          {Object.keys(states_abbr).map((state) => {
+          {Object.keys(states_abbr).map((state, index) => {
             return (
-              <option value={state}>{states_abbr[state]}</option>
+              <option key={index} value={state}>{states_abbr[state]}</option>
             )
           })}
         </select>
@@ -250,31 +280,31 @@ function MyForm({loading, getSummary}) {
         </div>
         <div className='flex justify-between'>
           <div>
-            <input className='m-1' onChange={onCheckbox} type="checkbox" id='rating1' name="ratings" value="1" />
-            <label for="ratings1">1 Star</label>
+            <input className='m-1 input-bg' onChange={onCheckbox} type="checkbox" id='rating1' name="ratings" value="1" />
+            <label htmlFor="ratings1">1 Star</label>
           </div>
           <div>
-            <input className='m-1' onChange={onCheckbox} type="checkbox" id='rating2' name="ratings" value="2" />
-            <label for="ratings2">2 Star</label>
+            <input className='m-1 input-bg' onChange={onCheckbox} type="checkbox" id='rating2' name="ratings" value="2" />
+            <label htmlFor="ratings2">2 Star</label>
           </div>
           <div>
-            <input className='m-1' onChange={onCheckbox} type="checkbox" id='rating3' name="ratings" value="3" />
-            <label for="ratings3">3 Star</label>
+            <input className='m-1 input-bg' onChange={onCheckbox} type="checkbox" id='rating3' name="ratings" value="3" />
+            <label htmlFor="ratings3">3 Star</label>
           </div>
           <div>
-            <input className='m-1' onChange={onCheckbox} type="checkbox" id='rating4' name="ratings" value="4" />
-            <label for="ratings4">4 Star</label>
+            <input className='m-1 input-bg' onChange={onCheckbox} type="checkbox" id='rating4' name="ratings" value="4" />
+            <label htmlFor="ratings4">4 Star</label>
           </div>
           <div>
-            <input className='m-1' onChange={onCheckbox} type="checkbox" id='rating5' name="ratings" value="5" />
-            <label for="ratings5">5 Star</label>
+            <input className='m-1 input-bg' onChange={onCheckbox} type="checkbox" id='rating5' name="ratings" value="5" />
+            <label htmlFor="ratings5">5 Star</label>
           </div>
         </div>
         </fieldset>
         <div className='font-semibold'>
-        <label for='product'>Product/Service (optional)</label>
+        <label htmlFor='product'>Product/Service (optional)</label>
         </div>
-        <select onChange={productSelect} className='border p-2 rounded-s-sx' id="product" name='product'>
+        <select onChange={productSelect} className='border p-2 rounded-s-sx input-bg' id="product" name='product'>
           <option value="">Choose a Product/Service</option>
           <option disabled>------------------</option>
           <option value="mobile order">Mobile Order System</option>
@@ -307,9 +337,15 @@ function MyForm({loading, getSummary}) {
 
 
 
-function MySummary({summary}) {
+function MySummary({summary, isloading}) {
   return (
-      <div className='p-2 m-2 font-display font-medium text-lg'>
+      <div className='p-2 m-2 font-display font-medium text-lg data-numbers'>
+        {isloading ? 
+        <div className='loader'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div> : <></> }
         {summary}
       </div>
   )
@@ -386,6 +422,50 @@ function App() {
       <ContainerComp />
     </>
   )
+}
+
+function getlargestIncrease(arr) {
+  if (arr.length < 2) return null;
+
+  let maxIncrease = 0;
+  let index = -1;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    const increase = arr[i + 1] - arr[i];
+    if (increase > maxIncrease) {
+      maxIncrease = increase;
+      index = i; // index of the first element in the pair
+    }
+  }
+
+  return {
+    maxIncrease,
+    from: arr[index],
+    to: arr[index + 1],
+    increasingindices: [index, index + 1],
+  };
+}
+
+function getlargestDecrease(arr) {
+  if (arr.length < 2) return null;
+
+  let maxDecrease = 0;
+  let index = -1;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    const decrease = arr[i] - arr[i + 1];
+    if (decrease > maxDecrease) {
+      maxDecrease = decrease;
+      index = i; // index of the first element in the pair
+    }
+  }
+
+  return {
+    maxDecrease,
+    from: arr[index],
+    to: arr[index + 1],
+    decreasingindices: [index, index + 1],
+  };
 }
 
 export default App
